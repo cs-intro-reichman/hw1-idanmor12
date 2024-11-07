73abed6398bf6c522f97a21e607acb3a1c09de06
Idan Mor <idan.mor@post.runi.ac.il>
2024-11-07 16:19:21 +0200

    Completed HW1

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,10 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int numberInput = Integer.parseInt(args[0]);
+		int onesDigit= numberInput % 10;
+		int tensDigit= (numberInput / 10) %10;
+		int hundredsDigit = (numberInput / 100);
+		System.out.println(hundredsDigit + " hundreds, " + tensDigit + " tens, and " + onesDigit + " ones.");
 	}
 }
