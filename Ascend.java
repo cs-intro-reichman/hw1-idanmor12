6f7c423c1ad7d89e0f92fdf8cf0975aa58143ad5
Idan Mor <idan.mor@post.runi.ac.il>
2024-11-09 19:38:29 +0200

    ascend 2

--- Ascend.java
+++ Ascend.java
@@ -5,7 +5,7 @@ public class Ascend {
 		int lim = Integer.parseInt(args[0]);
 		int a = (int) (Math.random() * lim);
 		int b = (int) (Math.random() * lim);
-		int c = (int) (Math.random() * lim);
+		int c = (int) (Math.random() * lim);  
 
 		int min = Math.min(a, Math.min(b, c));
 		int max = Math.max(a, Math.max(b, c)); 
